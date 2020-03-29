a1502a007fa88aa4669015856f19f4aa1eed15e3
Axing <43921905+LXxxx1@users.noreply.github.com>
2020-03-29 23:19:29 +0800

    delete all

--- config/apiUrl.js
+++
@@ -1,8 +0,0 @@
-const ipUrl = 'http://127.0.0.1:7001/default/'
-
-export const serviceUrl = {
-  getArticleList: ipUrl + 'getArticleList', //首页数据
-  getArticleById: ipUrl + 'getArticleById/', // 根据id获取文章详情
-  getTypeInfo: ipUrl + 'getTypeInfo',       // 文航类别
-  getListById: ipUrl + 'getListById/'        // 根据类别获取文章列表
-} 
