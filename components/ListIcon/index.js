88629c2bf4b48e0a6972ab6e2c9a0a6fe87df102
Axing <43921905+LXxxx1@users.noreply.github.com>
2020-06-22 03:26:38 +0800

    ren

--- components/ListIcon/index.js
+++ components/ListIcon/index.js
@@ -1,33 +1,37 @@
 import React from 'react';
-import CountTo from 'react-count-to';
 import { Tag } from 'antd'
 import ViewCount from '../../components/ViewCount'
 import './index.css'
 import {
-  CalendarOutlined ,
+  CalendarOutlined,
   FileOutlined,
   FireTwoTone
 } from '@ant-design/icons'
 
-const style = {
-  display: 'inline-block',
-  padding: '0 10px'
-}
-const ListIcon = ({ item, className = '',isTop = false }) => {
+const style = (istop = false) => ({
+    display: 'inline-block',
+    padding: '0 20px',
+  })
+  
+
+
+
+
+const ListIcon = ({ item, className = '', isTop = false }) => {
   return (
     <div className={`list-icon ${className}`}>
-      { isTop ? <Tag style={style} color='red'>置顶</Tag> : null}
+      {isTop ? <Tag style={style()} color='red'>置顶</Tag> : null}
       <span>
         {/* <CarryOutOutlined style={{ color: '#4091F7' }} />{item.addTime} */}
         <CalendarOutlined />{item.addTime}
-        
+
       </span>
       <span>
         {/* <FileOutlined style={{ color: 'gold' }} />{item.typeName} */}
-        <FileOutlined  />{item.typeName}
+        <FileOutlined />{item.typeName}
       </span>
       <span>
-        <FireTwoTone twoToneColor='#f0732e' /><ViewCount value={item.view_count} />次点击
+        <FireTwoTone twoToneColor='#f0732e' /><ViewCount value={item.view_count} />人
       </span>
     </div>
 
